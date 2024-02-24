dacbc77307ebf3e630811c248153a69223f92f6e
Giovanni Bernardes Figueiredo <giovannibernardesfigueiredo@gmail.com>
2024-02-24 17:17:52 -0300

    Criado um servidor no pgadimn, rodando em uma imagem docker na porta 54321.

---
+++ migration/docker-database-initial.sql
@@ -0,0 +1,11 @@
+create table personalidades(
+    id serial primary key,
+    nome varchar,
+    historia varchar
+);
+
+INSERT INTO personalidades(nome, historia) VALUES
+('Deodato Petit Wertheimer', 'Rui Barbosa recebeu o cognome de "Águia de Haia" do Barão do Rio Branco que era o Ministro das Relações Exteriores na época da
+ 2ª Conferência Internacional da Paz em 1907 "um diplomata de grandes qualidades e também um excelente criador de slogans", segundo dr. Américo Jacobina Lacombe.'),
+('Carmela Dutra', 'Carmela Teles Leite Dutra foi a primeira-dama do Brasil, de 31 de janeiro de 1946 até a sua morte, tendo sido a esposa de Eurico Gaspar Dutra, 
+16.º Presidente do Brasil. Era, carinhosamente, chamada de Dona Santinha, pela sua forte religiosidade, fazendo seu marido abrir uma capelinha no Palácio Guanabara.');
