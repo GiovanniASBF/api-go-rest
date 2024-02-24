3ca0ab639fb66dcafdb0f85874e7f0f0ce248d42
Giovanni Bernardes Figueiredo <giovannibernardesfigueiredo@gmail.com>
2024-02-24 17:56:48 -0300

    GORM instalado, juntamente com o driver do Postgres. Criado arquivo db resposavel por conectar com o banco de dados. Estabelecida a conexao com o banco. Funcoes do controller ajustados.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/GiovanniASBF/api-go-rest/database"
 	"github.com/GiovanniASBF/api-go-rest/models"
 	"github.com/GiovanniASBF/api-go-rest/routes"
 )
@@ -12,7 +13,7 @@ func main() {
 		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
 		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
 	}
-
+	database.ConectaComBancoDeDados()
 	fmt.Println("Iniciando o servidor Rest com Go")
 	routes.HandleRequest()
 }
