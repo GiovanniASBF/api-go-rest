bfd03a1b0f152d49daa626c0d8571ccf7f370dc3
Giovanni Bernardes Figueiredo <giovannibernardesfigueiredo@gmail.com>
2024-02-24 10:00:41 -0300

    Recebendo requisicao e abrindo pagina Home

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func main() {
+	fmt.Println("Iniciando o servidor Rest com Go")
+	HandleRequest()
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Home Page")
+}
+
+func HandleRequest() {
+	http.HandleFunc("/", Home)
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
