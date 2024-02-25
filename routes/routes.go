23b09264b3df20108270de2089acb1539f4e5235
Giovanni Bernardes Figueiredo <giovannibernardesfigueiredo@gmail.com>
2024-02-25 09:13:12 -0300

    Adicionado um middleware. Content-Type Json definido para as respostas das requisicoes, melhorando a apresentacao.

--- routes/routes.go
+++ routes/routes.go
@@ -5,11 +5,13 @@ import (
 	"net/http"
 
 	"github.com/GiovanniASBF/api-go-rest/controllers"
+	"github.com/GiovanniASBF/api-go-rest/middleware"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
